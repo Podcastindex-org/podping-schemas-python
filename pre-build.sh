306ec12e6627e5e3244c0643cfbfa438b55f088b
Alecks Gates <alecks@podping.org>
2022-10-25 21:40:46 -0500

    Update misc comments in pre-build script

--- pre-build.sh
+++ pre-build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-#version="$1"
-
 # Since we have to install epel-release under yum first, install capnproto here
 # Need this if the version of manylinux used doesn't included epel-release
+# Like the almalinux 8 base
 #if command -v apt-get >/dev/null; then
 #    apt-get update
 #    apt-get install -y capnproto  || { echo "Installing custom apt package(s) failed."; exit 1; }
@@ -16,5 +15,3 @@
 # Remove build artifacts from previous version
 # Otherwise we get multiple version .so files, creating large wheels
 find ./ -type f \( -iname '*.so' -o -iname '*.pyx' -o -iname '*.c' \) -delete
-
-#sed -i -e 's/"capnpy"]/"https:\/\/github.com\/agates\/capnpy\/releases\/download\/0.9.1dev0\/capnpy-0.9.1.dev0-'"$version"'.whl"]/g' pyproject.toml
